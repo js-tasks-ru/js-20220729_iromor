218f3c6a2a131a7ff6546be81a532425e5f6309e
ilya <ichaplin@yandex.ru>
2022-08-09 11:24:52 +0300

    cant do trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -6,4 +6,25 @@
  */
 export function trimSymbols(string, size) {
 
+  if (size === 0) { return ''; }
+  let result = [];
+  let count = size;
+  const arr = string.split('');
+
+  const ok = {
+    [arr[0]]: true
+  };
+
+  arr.forEach((el, i) => {
+    if (ok.el && count < i) {
+      result.push(el);
+    }
+    if (count === i) {
+      count += count;
+      ok.el = false;
+    } else {
+      result.push(el);
+    }
+  });
+  return result.join('');
 }
